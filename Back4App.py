3488e758179d816566627bf55b9093e6b676e44b
Velmurugan Moorthy <velmurugan1993@gmail.com>
2020-05-15 23:16:31 +0530

    Vel | Fetching poems from the Back4App DB

---
+++ Back4App.py
@@ -0,0 +1,40 @@
+import requests
+import json
+#from local_settings import *
+
+
+def get_headers():
+    return {
+        'X-Parse-Application-Id': '',
+        'X-Parse-REST-API-Key': '',
+        'Content-Type': 'application/json'
+    }
+
+
+def update_status(objectId):
+    url = "https://parseapi.back4app.com/classes/WordCorpus/"+objectId
+    payload = {'status': True}
+    header = get_headers()
+
+    response = requests.put(url, data=json.dumps(payload), headers=header)
+    print(response.text)
+    return response.status_code
+
+
+class Back4AppTools():
+    def get_song(self):
+        header = get_headers()
+        url = "https://parseapi.back4app.com/classes/KurunthogaiPoems"
+        data = requests.get(url, headers=header)
+        json_response = data.json()
+
+        results = json_response['results'][0]
+        poem_with_author = ('பாடல் : %s \n ~ %s' %
+                    (results['KurunthogaiPoem'], results['PoemAuthor']))
+        #update_status(results['objectId'])
+        tags = '\n#தினமொரு_குறுந்தொகை'
+        return poem_with_author+tags
+
+
+if __name__ == "__main__":
+    print("Try running daily_one_word.py first")
