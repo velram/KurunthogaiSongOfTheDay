755c1368286bf544fb3430580bc9c03bfc6bd421
Velmurugan Moorthy <velmurugan1993@gmail.com>
2020-05-16 07:34:48 +0530

    Vel | Scraping Kurunthogai URLs - moved code into a class and new file

--- Daily_One_Kurunthogai.py
+++ Daily_One_Kurunthogai.py
@@ -1,7 +1,14 @@
 import Back4App
+import kurunthogai_poem_urls_scrapper
 
-back4app_tools = Back4App.Back4AppTools()
 
-kurunthogai_song = back4app_tools.get_song()
+FIRST_KURUNTHOGAI_PAGE_URL = 'https://ta.wikisource.org/s/s6' # Song 1 to 10 
 
-print("kurunthogai_song : \n ", kurunthogai_song)
+#back4app_tools = Back4App.Back4AppTools()
+#kurunthogai_song = back4app_tools.get_song()
+
+kurunthogai_url_scrapper = kurunthogai_poem_urls_scrapper.Kurunthogai_URL_Scrapper()
+kurunthogai_urls = kurunthogai_url_scrapper.get_kurunthogai_page_links(FIRST_KURUNTHOGAI_PAGE_URL)
+
+#print("kurunthogai_song : \n ", kurunthogai_song)
+print("Kurunthogai URLs : ", kurunthogai_urls)
--- Kurunthogai_URL_Scrapper.py
+++ Kurunthogai_URL_Scrapper.py
@@ -16,6 +16,8 @@ FIRST_KURUNTHOGAI_PAGE_URL = 'https://ta.wikisource.org/s/s6' # Song 1 to 10
 #url = 'https://ta.wikisource.org/s/zc' #Song 101 to 110
 
 
+class Kurunthogai_URL_Scrapper:
+
 def get_beautiful_soup_object(page_url):
 	# Connect to the URL
 	response = requests.get(page_url)
@@ -47,6 +49,7 @@ def get_kurunthogai_page_links(first_page_url):
 			#print("next song list text : " + next_song_list_link_text + " path : "+next_song_list_path)
 			if(None != next_song_list_path):
 				next_song_absolute_url = WIKISOURCE_URL_ROOT + next_song_list_path
+				kurunthogai_song_urls.append(next_song_absolute_url)
 				print(next_song_list_link_text + " : " + next_song_absolute_url + "\n")
 				soup_object = get_beautiful_soup_object(next_song_absolute_url)
 			if(None == next_song_absolute_url or first_page_url == next_song_absolute_url):
@@ -55,8 +58,8 @@ def get_kurunthogai_page_links(first_page_url):
 		else:
 			print("Empty song container or No links found")
 			break
-	return None			
+	return kurunthogai_song_urls			
 
-get_kurunthogai_page_links(FIRST_KURUNTHOGAI_PAGE_URL)
+kurunthogai_poem_urls = get_kurunthogai_page_links(FIRST_KURUNTHOGAI_PAGE_URL)
 
-print("Program ends")
+print("Kurunthogai URLs : ", kurunthogai_poem_urls)
