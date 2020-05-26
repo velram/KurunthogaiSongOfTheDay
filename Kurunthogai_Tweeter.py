3f47dda9a08471f0dceb29ed38221bbd0fd30424
Velmurugan Moorthy <velmurugan1993@gmail.com>
2020-05-26 18:59:14 +0530

    Vel - Code refactoring - unused code removed

--- Kurunthogai_Tweeter.py
+++ Kurunthogai_Tweeter.py
@@ -1,12 +1,6 @@
 import twitter
 import Back4App
 
-
-# API key : mJTddacPiqxwwKqpCgkPEEc5a
-# API secret key : URaMze2c3HGF8vUQh0k5BooXHaaeZGxFAkqCumFGkNjEEGY6kf
-# Access token : 1068651616616701953-EvlNpFt1d8a4IbLufl0XDs0x07QSX4
-# Access token secret : 7JYhhceh7i215CFTSVWlhAOdFOMMzYlL1wCcF6S4FgTrv
-
 TWITTER_API_KEY = ''
 TWITTER_API_SECRET_KEY = ''
 TWITTER_ACCESS_TOKEN_KEY = ''
@@ -14,10 +8,6 @@ TWITTER_ACCESS_TOKEN_SECRET = ''
 
 back4app_tools = Back4App.Back4AppTools()
 todays_kurunthogai = back4app_tools.get_song()
-
-
-# Access token : 134152924-UJBQnTFnkVRCy57U1N2pcizObVJyLxX7sjg5ZvCO
-# Access token (secret) : 32uSIj2L9TJ4zDaHT54qvqraINCve03Vn9jmetaXvQQyl
 
 def connect_twitter():
     return twitter.Api(consumer_key=TWITTER_API_KEY,
